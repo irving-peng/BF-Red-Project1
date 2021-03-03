6ddae4c4c2aa07184a9e7b1ce37bdf3d8c1be524
carterlindstrom <79881745+carterlindstrom@users.noreply.github.com>
2021-03-02 21:47:21 -0600

    Add files via upload

--- Backend.java
+++ Backend.java
@@ -1,26 +1,77 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.zip.DataFormatException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
-public class Backend{
+public class Backend implements BackendInterface{
   private ArrayList<String> Genres;
   private int size;
-  private HashTableMap<String, MovieObject> bigTable;
+  private HashTableMap<String, MovieInterface> bigTable;
   private ArrayList<String> inList;
   private ArrayList<String> keys;
   private ArrayList<Integer> Ratings;
   private ArrayList<String> allGenres;
-
-  public Backend(String FilePath) throws FileNotFoundException {
-    FileReader file = new FileReader(FilePath);
+  public Backend(FileReader FileRead){
+    List<MovieInterface> BigList = null;
     MovieDataReaderInterface read = new MovieDataReader();
-    List<MovieObject> BigList = read.readDataSet(file);
-    bigTable = new HashTableMap<String, MovieObject>(BigList.size()*2);
+    try {
+    BigList = read.readDataSet(FileRead);
+    } catch (IOException DataFormatException) { System.out.println("File format error");
+    return;}
+    catch(DataFormatException e) {
+      System.out.println("Data format error");
+    }
+    bigTable = new HashTableMap<String, MovieInterface>(BigList.size()*2);
     keys = new ArrayList<String>(BigList.size());
     for (int i=0; i<BigList.size(); i++) {
-      MovieObject current = BigList.get(i);
+      MovieInterface current = BigList.get(i);
+      String key = "";
+      for (int a=0; a<current.getGenres().size(); a++) key.concat(current.getGenres().get(a));
+      key.concat(current.getTitle());
+      key.concat(current.getDirector());
+      bigTable.put(key, current);
+      keys.add(key);
+      Iterator<String> itGenre = current.getGenres().iterator();
+      String cGenre;
+      allGenres = new ArrayList<String>(10);
+      int gcount=0;
+      while (itGenre.hasNext()) {
+        cGenre = itGenre.next();
+        if (!allGenres.contains(cGenre)) {
+          allGenres.add(cGenre); 
+          gcount++;}
+        if (gcount+2 == allGenres.size()) {
+          ArrayList<String> newAllGenres = new ArrayList<String>((gcount+2)*2);
+          Iterator<String> genit2 = allGenres.iterator();
+          while (genit2.hasNext()) newAllGenres.add(genit2.next());
+          allGenres = newAllGenres;
+        }
+        }
+      }
+      inList = new ArrayList<String>(BigList.size());
+      Genres = new ArrayList<String>(10);
+      ArrayList<Integer> Ratings = new ArrayList<Integer>(11);
+      size = 0;
+      
+    
+  }
+  public Backend(String FilePath) throws FileNotFoundException {
+    List<MovieInterface> BigList = null;
+    FileReader file = new FileReader(FilePath);
+    MovieDataReaderInterface read = new MovieDataReader();try {
+      BigList = read.readDataSet(file);
+      } catch (IOException DataFormatException) { System.out.println("File format error");
+      return;}
+    catch(DataFormatException e) {
+      System.out.println("Data format error");
+    }
+    bigTable = new HashTableMap<String, MovieInterface>(BigList.size()*2);
+    keys = new ArrayList<String>(BigList.size());
+    for (int i=0; i<BigList.size(); i++) {
+      MovieInterface current = BigList.get(i);
       String key = "";
       for (int a=0; a<current.getGenres().size(); a++) key.concat(current.getGenres().get(a));
       key.concat(current.getTitle());
@@ -52,18 +103,18 @@ public class Backend{
     
   }
   private void sweep() {
-    MovieObject currentFilm;
+    MovieInterface currentFilm;
     boolean validGenre =false;
     for (int i=0; i<bigTable.size(); i++) {
       currentFilm = bigTable.get(keys.get(i));
-      Iterator genreIt  = Genres.iterator();
+      Iterator<String> genreIt  = Genres.iterator();
       while (genreIt.hasNext()) {
         if (Genres.contains(genreIt.next())) validGenre=true;
       }
       if (validGenre) {
         validGenre=false;
         if (Ratings.contains((Integer) currentFilm.getAvgVote().intValue())) {
-          if (!inList.contains(currentFilm)) {
+          if (!inList.contains(keys.get(i))) {
             inList.add(keys.get(i));
             size++;
           }
@@ -95,7 +146,7 @@ public class Backend{
   private void cleanup() {
     Iterator<String> itFilms = inList.iterator();
     int index = 0;
-    MovieObject current;
+    MovieInterface current;
     boolean genreMatch = false;
     Iterator<String> itgenre;
     while (itFilms.hasNext()) {
@@ -140,8 +191,23 @@ public class Backend{
     return size;
     
   }
-  public List<MovieObject> getThreeMovies(int startingIndex) {
-    return null;
+  public List<MovieInterface> getThreeMovies(int startingIndex) {
+    Iterator<String> Mvit = keys.iterator();
+    ArrayList<MovieInterface> finalM = new ArrayList<MovieInterface>(3);
+    ArrayList<MovieInterface> workingM = new ArrayList<MovieInterface>(keys.size());
+    while (Mvit.hasNext()) {
+      String currentKey = Mvit.next();
+    for (int i=0; i<3; i++) {
+      if (workingM.get(i) == null) workingM.add(bigTable.get(currentKey));
+      else {
+        if (bigTable.get(currentKey).getAvgVote()>workingM.get(i).getAvgVote()) {
+          workingM.add(i,bigTable.get(currentKey));
+        }
+      }
+    }
+    }
+    for (int a=0; a<3; a++) finalM.set(a, workingM.get(a));
+    return finalM;
   }
   public List<String> getAllGenres() {
     return allGenres;
